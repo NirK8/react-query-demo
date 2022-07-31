e856c4733cb1884cb03dc9029572e7ea735f9623
Nir Kahana <nir@moveohls.com>
2022-07-31 16:22:37 +0300

    changed git dir

--- server/src/users/users.schema.ts
+++ server/src/users/users.schema.ts
@@ -1,39 +1,39 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { IUser, Role, RoleClass } from '@protecht/types';
+// import { IUser, Role, RoleClass } from '@protecht/types';
 
-enum DocumentStatus {
-  ACTIVE = 0,
-  INACTIVE,
-}
-export type UsersDocument = IUser;
+// enum DocumentStatus {
+//   ACTIVE = 0,
+//   INACTIVE,
+// }
+// export type UsersDocument = IUser;
 
-@Schema({ timestamps: true })
-export class User {
-  @Prop({ required: true })
-  firstName: string;
-  @Prop({ required: true })
-  lastName: string;
-  @Prop()
-  phone?: string;
-  @Prop()
-  email: string;
-  @Prop()
-  address?: string;
-  @Prop()
-  sub?: string;
-  @Prop({ type: RoleClass })
-  role: Role;
-  @Prop({ default: DocumentStatus.ACTIVE, enum: DocumentStatus, type: Number })
-  status: 0 | 1;
-}
+// @Schema({ timestamps: true })
+// export class User {
+//   @Prop({ required: true })
+//   firstName: string;
+//   @Prop({ required: true })
+//   lastName: string;
+//   @Prop()
+//   phone?: string;
+//   @Prop()
+//   email: string;
+//   @Prop()
+//   address?: string;
+//   @Prop()
+//   sub?: string;
+//   @Prop({ type: RoleClass })
+//   role: Role;
+//   @Prop({ default: DocumentStatus.ACTIVE, enum: DocumentStatus, type: Number })
+//   status: 0 | 1;
+// }
 
-export const UsersSchema = SchemaFactory.createForClass(User);
+// export const UsersSchema = SchemaFactory.createForClass(User);
 
-UsersSchema.methods.toJSON = function () {
-  const obj = this.toObject();
-  delete obj.__v;
-  delete obj.createdAt;
-  delete obj.updatedAt;
+// UsersSchema.methods.toJSON = function () {
+//   const obj = this.toObject();
+//   delete obj.__v;
+//   delete obj.createdAt;
+//   delete obj.updatedAt;
 
-  return obj;
-};
+//   return obj;
+// };
